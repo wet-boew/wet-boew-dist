290b883db7df2ba88a63c932c1a6c8eba3e795a5
Travis <laurent.goderre@gmail.com>
2013-05-16 05:37:30 +0000

    Travis build 3794 pushed to v3.0

--- dist/js/settings.js
+++ dist/js/settings.js
@@ -3,7 +3,7 @@
  * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
  * wet-boew.github.io/wet-boew/License-eng.txt / wet-boew.github.io/wet-boew/Licence-fra.txt
  *
- * Version: v3.0.7-release Build: 2013-05-16 04:34 AM
+ * Version: v3.0.8-development Build: 2013-05-16 05:35 AM
  *
  */
 /*
