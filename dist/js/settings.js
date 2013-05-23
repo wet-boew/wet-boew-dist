0ed0903d4e54f0b64ca8696e8a16fdcf023eaa4a
Travis <laurent.goderre@gmail.com>
2013-05-23 16:39:03 +0000

    Travis build 3835 pushed to v3.0

--- dist/js/settings.js
+++ dist/js/settings.js
@@ -3,7 +3,7 @@
  * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
  * wet-boew.github.io/wet-boew/License-eng.txt / wet-boew.github.io/wet-boew/Licence-fra.txt
  *
- * Version: v3.0.8-development Build: 2013-05-23 04:21 PM
+ * Version: v3.0.8-development Build: 2013-05-23 04:37 PM
  *
  */
 /*
@@ -11,7 +11,7 @@ WET-BOEW-Settings
  */
 var wet_boew_properties = {
 	/** global plugins are called via a array of dependency names **/
-	globals : ['equalize', 'deselectradio', 'css3ie']
+	"globals" : ["equalize", "deselectradio", "css3ie"]
 };
 
 /*
@@ -20,7 +20,7 @@ Global overrides for individual components
 
 // Share widget
 var wet_boew_share = {
-	sites : ['del.icio.us', 'digg', 'facebook', 'google', 'linkedin', 'reddit', 'stumbleupon', 'twitter', 'yahoobuzz']
+	"sites" : ["del.icio.us", "digg", "facebook", "google", "linkedin", "reddit", "stumbleupon", "twitter", "yahoobuzz"]
 };
 
 // Charts widget 
