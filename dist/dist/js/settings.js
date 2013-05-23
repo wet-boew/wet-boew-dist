c3abba2d5f6669057f81112c04a9a03a78ab7871
Travis <laurent.goderre@gmail.com>
2013-05-23 16:46:17 +0000

    Travis build 3836 pushed to master

--- dist/dist/js/settings.js
+++ dist/dist/js/settings.js
@@ -3,15 +3,15 @@
  * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
  * wet-boew.github.io/wet-boew/License-eng.txt / wet-boew.github.io/wet-boew/Licence-fra.txt
  *
- * Version: v3.1.2-development Build: 2013-05-23 04:31 PM
+ * Version: v3.1.2-development Build: 2013-05-23 04:42 PM
  *
  */
 /*
 WET-BOEW-Settings
  */
 var wet_boew_properties = {
-	/** global plugins are called via an array of dependency names **/
-	globals : ['deselectradio', 'datemodified']
+	/** global plugins are called via a array of dependency names **/
+	"globals" : ["deselectradio", "datemodified"]
 };
 
 /*
@@ -20,5 +20,5 @@ Global overrides for individual components
 
 // Share widget
 var wet_boew_share = {
-	sites : ['del.icio.us', 'digg', 'facebook', 'google', 'linkedin', 'reddit', 'stumbleupon', 'twitter', 'yahoobuzz']
+	"sites" : ["del.icio.us", "digg", "facebook", "google", "linkedin", "reddit", "stumbleupon", "twitter", "yahoobuzz"]
 };
