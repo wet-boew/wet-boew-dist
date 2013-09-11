0760e9acb335cd8512adb4f29ed5da0adc7fdbf4
Travis <laurent.goderre@gmail.com>
2013-09-11 01:51:08 +0000

    Travis build 4591

--- dist/js/settings.js
+++ dist/js/settings.js
@@ -3,12 +3,14 @@
  * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
  * wet-boew.github.io/wet-boew/License-en.html / wet-boew.github.io/wet-boew/Licence-fr.html
  *
- * Version: v3.0.10-development Build: 2013-09-10 04:37 PM EDT
+ * Version: v3.0.10-development Build: 2013-09-10 09:49 PM EDT
  *
  */
 /*
 WET-BOEW-Settings
  */
+
+/* exported wet_boew_properties, wet_boew_share */
 var wet_boew_properties = {
 	/** global plugins are called via a array of dependency names **/
 	"globals" : ["equalize", "deselectradio", "css3ie"]
