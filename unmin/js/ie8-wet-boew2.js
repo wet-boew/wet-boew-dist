f34c727ab8e0f4fd332cf24a68fb94202366fb29
Web <wet.boew.bot@gmail.com>
2014-11-05 20:50:33 +0000

    Travis build 9847

--- unmin/js/ie8-wet-boew2.js
+++ unmin/js/ie8-wet-boew2.js
@@ -1,7 +1,7 @@
 /*!
  * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
  * wet-boew.github.io/wet-boew/License-en.html / wet-boew.github.io/wet-boew/Licence-fr.html
- * v4.0.8-development - 2014-11-04
+ * v4.0.8-development - 2014-11-05
  *
  *//**
  * @title WET-BOEW JQuery Helper Methods
@@ -4587,6 +4587,7 @@ var componentName = "wb-feeds",
 	initEvent = "wb-init" + selector,
 	$document = wb.doc,
 	patt = /\\u([\d\w]{4})/g,
+	limitTypes = [ "load", "display" ],
 
 	/**
 	 * @object Templates
@@ -4706,15 +4707,19 @@ var componentName = "wb-feeds",
 	/**
 	 * Helper function that returns a class-based set limit on plugin instances
 	 * @method getLimit
-	 * @param {DOM object} elm The element to search for a class of the form limit-5
+	 * @param {DOM object} elm The element to search for a class of the form {limit-type}-5
+	 * @param {string} type The type of limit ("load" or "display")
 	 * @return {number} 0 if none found, which means the plugin default
 	 */
-	getLimit = function( elm ) {
-		var count = elm.className.match( /\blimit-\d+/ );
-		if ( !count ) {
+	getLimit = function( elm, type ) {
+		var re = new RegExp( "\\b" + type + "-(\\d+)", "i" ),
+			limit = elm.className.match( re );
+
+		if ( !limit ) {
 			return 0;
 		}
-		return Number( count[ 0 ].replace( /limit-/i, "" ) );
+
+		return Number( limit[ 1 ] );
 	},
 
 	/**
@@ -4746,18 +4751,27 @@ var componentName = "wb-feeds",
 		// returns DOM object = proceed with init
 		// returns undefined = do not proceed with init (e.g., already initialized)
 		var elm = wb.init( event, componentName, selector ),
-			fetch, url, $content, limit, feeds, fType, last, i, callback, fElem, fIcon;
+			fetch, url, $content, loadLimit, displayLimit, feeds, fType, last, i, callback, fElem, fIcon;
 
 		if ( elm ) {
 			$content = $( elm ).find( ".feeds-cont" );
-			limit = getLimit( elm );
+			loadLimit = getLimit( elm, limitTypes[ 0 ] );
+			displayLimit = getLimit( elm, limitTypes[ 1 ] );
 			feeds = $content.find( feedLinkSelector );
 			last = feeds.length - 1;
+
+			// Ensure load and display limits are either non-zero or both zero
+			if ( loadLimit === 0 && displayLimit !== 0 ) {
+				loadLimit = displayLimit;
+			} else if ( displayLimit === 0 && loadLimit !== 0 ) {
+				displayLimit = loadLimit;
+			}
 
 			// Lets bind some variables to the node to ensure safe ajax thread counting
 
 			$content.data( "toProcess", feeds.length )
-					.data( "feedLimit", limit )
+					.data( "loadLimit", loadLimit )
+					.data( "displayLimit", displayLimit )
 					.data( "entries", [] );
 
 			for ( i = last; i !== -1; i -= 1 ) {
@@ -4785,7 +4799,7 @@ var componentName = "wb-feeds",
 					fetch.url = fElem.attr( "data-ajax" );
 					fetch.jsonp = callback;
 				} else {
-					url = jsonRequest( fElem.attr( "href" ), limit );
+					url = jsonRequest( fElem.attr( "href" ), loadLimit );
 					fetch.url = url;
 
 					// Let's bind the template to the Entries
@@ -4840,7 +4854,7 @@ var componentName = "wb-feeds",
 		entries = $.merge( entries, $content.data( "entries" ) );
 
 		if ( toProcess === 1 ) {
-			parseEntries( entries, $content.data( "feedLimit" ), $content, this.feedType );
+			parseEntries( entries, $content.data( "displayLimit" ), $content, this.feedType );
 			return 0;
 		}
 
